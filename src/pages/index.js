64d0535377035277cdd48474702e9e1e5229630a
Maghin <ahmedshabana0.0@gmail.com>
2020-02-14 02:08:01 +0200

    looking for breakage on indx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 import Layout from '../components/layout';
+import Vshoot from '../components/vshoot';
+import Hshoot from '../components/hshoot';
+import Footer from '../components/footer';
 
-const IndexPage = () => <Layout />;
+import chejungle from '../images/chejungle.jpg';
 
+import chemotor from '../images/chemotor.jpg';
+import chemotor01 from '../images/chemotor01.jpg';
+
+const IndexPage = () => (
+  <div>
+    <Layout />
+    <Vshoot img={chejungle} number="01" shoot={'jungle'} />
+    <Hshoot img={chemotor} number="02" shoot={chemotor01} />
+    <Footer />
+  </div>
+);
 export default IndexPage;
